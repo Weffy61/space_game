6a9d319748165685babaf696405b323978ff3f54
Weffy61 <weffy@mail.ru>
2024-03-05 05:53:56 +0300

    Remove redundant canvas refresh

--- animations.py
+++ animations.py
@@ -45,7 +45,6 @@ async def animate_spaceship(canvas, row, column, max_row, max_column):
     frame2 = get_slide(os.path.join('frames', 'rocket_frame_2.txt'))
 
     for frame in cycle([frame1, frame2]):
-        canvas.border()
         frame_rows, frame_columns = get_frame_size(frame)
         rows_direction, columns_direction, space_pressed = read_controls(canvas)
         position_row = rows_direction + row
@@ -58,7 +57,6 @@ async def animate_spaceship(canvas, row, column, max_row, max_column):
             column = position_column
 
         draw_frame(canvas, row, column, frame)
-        canvas.refresh()
         await asyncio.sleep(0)
         draw_frame(canvas, row, column, frame, negative=True)
 
