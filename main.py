e4808ec61a3c7b454ad86e78ea99ce298770f7df
Weffy61 <weffy@mail.ru>
2024-03-06 01:08:52 +0300

    Format code

--- main.py
+++ main.py
@@ -16,19 +16,19 @@ def draw(canvas):
         blink(canvas,
               row=get_random_coord(max_row),
               column=get_random_coord(max_column),
-              symbol=get_random_symbol()) for _ in range(100)]
+              symbol=get_random_symbol())
+        for _ in range(100)]
     coroutines.append(
-                      fire(canvas,
-                           start_row=max_row - 2,
-                           start_column=max_column / 2 + 2,
-                           ))
-    coroutines.append(animate_spaceship(
-        canvas,
-        max_row / 2,
-        max_column / 2,
-        max_row,
-        max_column
-    ))
+        fire(canvas,
+             start_row=max_row - 2,
+             start_column=max_column / 2 + 2)
+    )
+    coroutines.append(
+        animate_spaceship(canvas,
+                          max_row / 2,
+                          max_column / 2,
+                          max_row,
+                          max_column))
     while True:
         for coroutine in coroutines.copy():
             try:
